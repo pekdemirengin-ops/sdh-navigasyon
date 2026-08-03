8b52a2ed0a592f94f8ac56264a207f40072c592e
pekdemirengin-ops <pekdemirengin@gmail.com>
2026-08-03 11:57:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ else:
         st.session_state["secilen_birim"] = "Seçim Yapınız..."
         
     secim = st.selectbox(
-        "Aradığınız Genel veya İdari Birimi seçiniz:", 
+        "Aradığınız DİĞER BİRİMLER seçiniz:", 
         liste_alan, 
         key="secilen_birim"
     )
